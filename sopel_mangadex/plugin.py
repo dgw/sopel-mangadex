ed3f220629a92ee6d7f3216af92c143f75fd9d00
dgw <dgw@technobabbl.es>
2025-01-19 21:28:44 -0600

    Add chapter link support
    The MangaDex API wrapper I found doesn't request any of the extra data,
    nor does it support passing the `includes[]` parameter when fetching a
    chapter (despite including that param in its method docstring), so the
    only way I could see to output a chapter's parent manga title was to
    fetch the manga object too.
    
    Sigh. But it does work, just inefficiently (two requests for something
    that should be doable in one).

--- sopel_mangadex/plugin.py
+++ sopel_mangadex/plugin.py
@@ -53,3 +53,42 @@ def manga_title_link(bot, trigger):
         ' | '.join(part for part in parts if part),  # skip empty strings
         truncation=' […]',
     )
+
+
+@plugin.url(r'https://mangadex.org/chapter/(?P<uuid>[0-9a-f-]+)(?:/(?P<page>[0-9]+))?')
+@plugin.output_prefix(OUTPUT_PREFIX)
+def manga_chapter_link(bot, trigger):
+    client = mangadex.series.Chapter()
+
+    try:
+        chapter = client.get_chapter_by_id(trigger.group('uuid'))
+    except md_errors.ApiError:
+        bot.reply('Error fetching chapter information')
+        return
+
+    client = mangadex.series.Manga()
+    try:
+        manga = client.get_manga_by_id(chapter.manga_id)
+    except md_errors.ApiError:
+        bot.reply('Error fetching manga information for chapter')
+        return
+
+    chapter_number = chapter.chapter
+    if chapter_number.is_integer():
+        chapter_number = int(chapter_number)
+
+    manga_title = manga.title.get('en', manga.title.get('ja', 'Unknown title'))
+
+    parts = []
+    parts.append(
+        getattr(chapter, 'title', '(unknown title)') or '(unknown title)'
+    )
+    if page := trigger.group('page'):
+        parts.append(f'Chapter {chapter_number}, page {page} of {manga_title}')
+    else:
+        parts.append(f'Chapter {chapter_number} of {manga_title}')
+
+    bot.say(
+        ' | '.join(part for part in parts if part),  # skip empty strings,
+        truncation=' […]',
+    )
